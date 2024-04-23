972d34cf6ee1d4d37e38705651c514b495ea4fcb
Ranjan2104 <amreshm@spanidea.com>
2024-04-23 15:40:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+//   console.log("re-render");
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       {`Count is ${count}`}
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           <button onClick={() => setCount(count + 1)}>Click</button>
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+import { signal } from '@preact/signals-react';
+const count = signal(0);
+const fname = signal("");
 
 function App() {
+  console.log("tt");
+  
   return (
     <div className="App">
       <header className="App-header">
+        <p>Count is {count}, {fname}</p>
+        <input onChange={(e) => fname.value = e.target.value}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={() => count.value++}>Click</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
